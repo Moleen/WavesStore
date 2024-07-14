449b2dc2b9ccbe28a5b5f8d8b248a1c8d5567fec
Moleen <maulana.syakhiya@gmail.com>
2024-07-14 08:10:50 +0700

    recode items

--- static/js/main/home.js
+++ static/js/main/home.js
@@ -1,25 +1,7 @@
 $(document).ready(function(){
-    $(".topup-joki").owlCarousel();
     $("#testi").owlCarousel();
   });
 
-  $('.topup-joki').owlCarousel({
-    loop:false,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:4
-        }
-    }
-    
-})
 
   $('#testi').owlCarousel({
     loop:false,
