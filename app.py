caed213aff6159f0534b8ba5c721d55fa013047e
Moleen <maulana.syakhiya@gmail.com>
2024-07-15 07:01:07 +0700

    menyelesaikan poage item dan menambahkan testi

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template('main/home.html')
 
+@app.route('/joki/<game>')
+def joki(game):
+    return render_template('main/joki.html')
+@app.route('/testimoni')
+def testimoni():
+    return render_template('main/testimoni.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
